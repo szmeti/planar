a96d7f2afc8cbf989659308c4a9493c4bba615ab
Janos Boroczki <janos.boroczki@corballis.ie>
2014-07-15 11:45:32 +0200

    dist merged

--- dist/planar.js
+++ dist/planar.js
@@ -2688,95 +2688,13 @@
             applyFilters(context.graph.getEdges(), context, filterEdges);
             context.executed = true;
         }
-        function filterVertices(vertex, context, filtered) {
-            if (!filtered) {
-                addVertexToGraph(context.normalGraph, vertex);
-                addVertexToGraph(context.aggregatedGraph, vertex);
-            } else {
-                context.filteredVertices.push(vertex);
-                context.filteredAggregatedVertices.push(vertex);
-            }
-        }
-        function addVertexToGraph(graph, vertex) {
-            var newVertex = graph.addVertex(vertex.id);
-            vertex.copyPropertiesTo(newVertex);
-        }
-        function addEdgeToGraph(graph, edge) {
-            var newEdge = graph.addEdge(edge.id, edge.outVertex, edge.inVertex, edge.label);
-            edge.copyPropertiesTo(newEdge);
-        }
-        function filterEdges(edge, context, filtered) {
-            var inVertex = context.normalGraph.getVertex(edge.inVertex.id);
-            var outVertex = context.normalGraph.getVertex(edge.outVertex.id);
-            var existingVertices = inVertex !== null && outVertex !== null;
-            if (existingVertices && !filtered) {
-                addEdgeToGraph(context.normalGraph, edge);
-                addAggregatedEdgeToGraph(context, edge);
-            } else {
-                addAggregatedEdgeToFilteredList(context, edge);
-                context.filteredEdges.push(edge);
-            }
-        }
-        function addAggregatedEdgeToFilteredList(context, edge) {
-            for (var i = 0; i < context.filteredEdges.length; i++) {
-                var filteredEdge = context.filteredEdges[i];
-                var connectsSameVertices = edge.connects(filteredEdge.inVertex, filteredEdge.outVertex);
-                if (connectsSameVertices) {
-                    var strength = edge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
-                    strength += filteredEdge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
-                    var newEdge = createSingleAggregatedEdge(filteredEdge.outVertex, filteredEdge.inVertex, strength, context);
-                    context.filteredAggregatedEdges.push(newEdge);
-                }
-            }
-        }
-        function createSingleAggregatedEdge(outVertex, inVertex, strength, context) {
-            var id = utils.generateId();
-            while (context.aggregatedGraph.getEdge(id) !== null) {
-                id = utils.generateId();
-            }
-            var newEdge = new Edge(id, outVertex, inVertex, null, context.aggregatedGraph);
-            newEdge.setProperty(settings.edge.lineWeightPropertyKey, strength);
-            return newEdge;
-        }
-        function addAggregatedEdgeToGraph(context, edge) {
-            var inVertex = context.aggregatedGraph.getVertex(edge.inVertex.id);
-            var outVertex = context.aggregatedGraph.getVertex(edge.outVertex.id);
-            var settings = context.graph.getSettings();
-            var strength = edge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
-            var inVertexEdges = inVertex.getEdges(BOTH);
-            if (inVertexEdges.length < 1) {
-                var newEdge = context.aggregatedGraph.addEdge(null, outVertex, inVertex);
-                newEdge.setProperty(settings.edge.lineWeightPropertyKey, strength);
-            } else {
-                findConnectedEdgesAndAggregate(inVertexEdges, outVertex, strength);
-            }
-        }
-        function findConnectedEdgesAndAggregate(sourceVertexEdges, destinationVertex, currentStrength) {
-            for (var i = 0; i < sourceVertexEdges.length; i++) {
-                var currentEdge = sourceVertexEdges[i];
-                var connectedWithDestinationVertex = currentEdge.inVertex === destinationVertex || currentEdge.outVertex === destinationVertex;
-                if (connectedWithDestinationVertex) {
-                    currentStrength += currentEdge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
-                    currentEdge.setProperty(settings.edge.lineWeightPropertyKey, currentStrength);
-                }
-            }
-        }
         function applyFilters(elements, context, filterCallback) {
-            if (!utils.isArray(elements)) {
-                elements = [ elements ];
-            }
             for (var i = 0; i < elements.length; i++) {
-                var currentElements = elements[i];
-                if (utils.isOfType(elements[i], Vertex) || utils.isOfType(elements[i], Edge)) {
-                    currentElements = [ currentElements ];
-                }
-                for (var id in currentElements) {
-                    var element = currentElements[id];
-                    var filters = context.filters;
-                    var operator = utils.isOfType(element, Vertex) ? context.vertexOperator : context.edgeOperator;
-                    var filtered = checkFiltered(element, filters, operator);
-                    filterCallback(element, context, filtered);
-                }
+                var element = elements[i];
+                var filters = context.filters;
+                var operator = utils.isOfType(element, Vertex) ? context.vertexOperator : context.edgeOperator;
+                var filtered = checkFiltered(element, filters, operator);
+                filterCallback(element, context, filtered);
             }
         }
         function checkFiltered(element, filters, operator) {
@@ -2804,6 +2722,85 @@
         }
         function filterCanBeApplied(element, filter) {
             return utils.isOfType(element, Vertex) && filter.type() === VERTEX_FILTER || utils.isOfType(element, Edge) && filter.type() === EDGE_FILTER || filter.type() === BOTH_FILTER;
+        }
+        function filterVertices(vertex, context, filtered) {
+            if (!filtered) {
+                addVertexToGraph(context.normalGraph, vertex);
+                addVertexToGraph(context.aggregatedGraph, vertex);
+            } else {
+                context.filteredVertices.push(vertex);
+                context.filteredAggregatedVertices.push(vertex);
+            }
+        }
+        function addVertexToGraph(graph, vertex) {
+            var newVertex = graph.addVertex(vertex.id);
+            vertex.copyPropertiesTo(newVertex);
+        }
+        function filterEdges(edge, context, filtered) {
+            var inVertex = context.normalGraph.getVertex(edge.getInVertex().id);
+            var outVertex = context.normalGraph.getVertex(edge.getOutVertex().id);
+            var existingVertices = inVertex !== null && outVertex !== null;
+            if (existingVertices && !filtered) {
+                addEdgeToGraph(context.normalGraph, edge);
+                addAggregatedEdgeToGraph(context, edge);
+            } else {
+                addAggregatedEdgeToFilteredList(context, edge);
+                context.filteredEdges.push(edge);
+            }
+        }
+        function addAggregatedEdgeToGraph(context, edge) {
+            var inVertex = context.aggregatedGraph.getVertex(edge.getInVertex().id);
+            var outVertex = context.aggregatedGraph.getVertex(edge.getOutVertex().id);
+            var settings = context.graph.getSettings();
+            var strength = edge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
+            var inVertexEdges = inVertex.getEdges(BOTH);
+            if (inVertexEdges.length < 1) {
+                var newEdge = context.aggregatedGraph.addEdge(null, outVertex, inVertex);
+                newEdge.setProperty(settings.edge.lineWeightPropertyKey, strength);
+            } else {
+                findConnectedEdgesAndAggregate(inVertexEdges, inVertex, outVertex, strength);
+            }
+        }
+        function addEdgeToGraph(graph, edge) {
+            var newEdge = graph.addEdge(edge.id, edge.getOutVertex(), edge.getInVertex(), edge.label);
+            edge.copyPropertiesTo(newEdge);
+        }
+        function addAggregatedEdgeToFilteredList(context, edge) {
+            var strength = edge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
+            var wasConnection = false;
+            for (var i = 0; i < context.filteredAggregatedEdges.length; i++) {
+                var filteredEdge = context.filteredAggregatedEdges[i];
+                var connectsSameVertices = edge.connects(filteredEdge.getInVertex(), filteredEdge.getOutVertex());
+                if (connectsSameVertices) {
+                    wasConnection = true;
+                    strength += filteredEdge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
+                    filteredEdge.setProperty(settings.edge.lineWeightPropertyKey, strength);
+                    break;
+                }
+            }
+            if (context.filteredAggregatedEdges.length < 1 || !wasConnection) {
+                var newEdge = createSingleAggregatedEdge(edge.getOutVertex(), edge.getInVertex(), strength, context);
+                context.filteredAggregatedEdges.push(newEdge);
+            }
+        }
+        function createSingleAggregatedEdge(outVertex, inVertex, strength, context) {
+            var id = utils.generateId();
+            while (context.aggregatedGraph.getEdge(id) !== null) {
+                id = utils.generateId();
+            }
+            var newEdge = new Edge(id, outVertex, inVertex, null, context.aggregatedGraph);
+            newEdge.setProperty(settings.edge.lineWeightPropertyKey, strength);
+            return newEdge;
+        }
+        function findConnectedEdgesAndAggregate(sourceVertexEdges, sourceVertex, destinationVertex, currentStrength) {
+            for (var i = 0; i < sourceVertexEdges.length; i++) {
+                var currentEdge = sourceVertexEdges[i];
+                var connectedWithDestinationVertex = currentEdge.connects(sourceVertex, destinationVertex);
+                if (connectedWithDestinationVertex) {
+                    currentStrength += currentEdge.getProperty(settings.edge.lineWeightPropertyKey) || settings.edge.defaultLineWeight;
+                    currentEdge.setProperty(settings.edge.lineWeightPropertyKey, currentStrength);
+                }
+            }
         }
         function resetFilterCounters(filters) {
             for (var i = 0; i < filters.length; i++) {
