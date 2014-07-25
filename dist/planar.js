819e928a16b4050438c3aa6d3b50e00af472c37e
Janos Boroczki <janos.boroczki@corballis.ie>
2014-07-25 16:51:33 +0200

    displayName has been added to Compare objects

--- dist/planar.js
+++ dist/planar.js
@@ -943,32 +943,38 @@
             EQUAL: {
                 evaluate: function(first, second) {
                     return first === second;
-                }
+                },
+                displayName: "Equal to"
             },
             NOT_EQUAL: {
                 evaluate: function(first, second) {
                     return first !== second;
-                }
+                },
+                displayName: "Not equal to"
             },
             GREATER_THAN: {
                 evaluate: function(first, second) {
                     return first !== null && second !== null && first > second;
-                }
+                },
+                displayName: "Greater than"
             },
             LESS_THAN: {
                 evaluate: function(first, second) {
                     return first !== null && second !== null && first < second;
-                }
+                },
+                displayName: "Less than"
             },
             GREATER_THAN_EQUAL: {
                 evaluate: function(first, second) {
                     return first !== null && second !== null && first >= second;
-                }
+                },
+                displayName: "Greater than or equal to"
             },
             LESS_THAN_EQUAL: {
                 evaluate: function(first, second) {
                     return first !== null && second !== null && first <= second;
-                }
+                },
+                displayName: "Less than or equal to"
             }
         };
     }();
