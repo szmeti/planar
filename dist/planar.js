05b1f171874949a2eb952e00986fd78e0f1da493
Gabor Szmetanko <gabor.szmetanko@corballis.ie>
2014-09-10 18:43:33 +0200

    Smoother drag and drop

--- dist/planar.js
+++ dist/planar.js
@@ -1658,30 +1658,20 @@
                 var drag = d3.behavior.drag();
                 var dragStartTime;
                 var instanceSettings = this.instanceSettings;
-                var zoom = this.zoom;
-                var dragOffsetX;
-                var dragOffsetY;
+                drag.origin(function(uiVertex) {'use strict';
+                    return uiVertex;
+                });
                 drag.on("dragstart", function(uiVertex) {
-                    var scale = zoom.scale();
-                    var translate = zoom.translate();
-                    if (scale === 1) {
-                        translate = [ 0, 0 ];
-                    }
-                    var sourceEvent = d3.event.sourceEvent;
-                    var translatedMouseX = sourceEvent.offsetX - translate[0];
-                    var translatedMouseY = sourceEvent.offsetY - translate[1];
-                    dragOffsetX = translatedMouseX / scale - uiVertex.x;
-                    dragOffsetY = translatedMouseY / scale - uiVertex.y;
                     dragStartTime = new Date().valueOf();
-                    sourceEvent.stopPropagation();
+                    d3.event.sourceEvent.stopPropagation();
                     var graph = uiVertex.vertex.getGraph();
                     graph.trigger("vertexDragStart", uiVertex);
                 });
                 drag.on("drag", function(uiVertex) {
                     var now = new Date().valueOf();
                     if (now - dragStartTime > instanceSettings.drag.delay) {
-                        uiVertex.x = d3.event.x - dragOffsetX;
-                        uiVertex.y = d3.event.y - dragOffsetY;
+                        uiVertex.x = d3.event.x;
+                        uiVertex.y = d3.event.y;
                         var graph = uiVertex.vertex.getGraph();
                         graph.trigger("vertexDrag", uiVertex);
                     }
