b1428b0f5eaf2de441f614d433966874b60ac398
Akos <akos.bordas@corballis.ie>
2015-08-11 10:36:29 +0200

    collision detection added to vertices with the drawing area, so that vertices cannot be dragged out of the canvas

--- src/view/d3/D3VertexManager.js
+++ src/view/d3/D3VertexManager.js
@@ -27,8 +27,49 @@ var D3VertexManager = (function () {
       drag.on('drag', function (uiVertex) {
         var now = new Date().valueOf();
         if (now - dragStartTime > instanceSettings.drag.delay) {
-          uiVertex.x = d3.event.x;
-          uiVertex.y = d3.event.y;
+          var vertexBoundingBox = uiVertex.g[0][0].getBBox();
+
+          var oldLeftCorner = {
+            x: uiVertex.x + vertexBoundingBox.x,
+            y: uiVertex.y + vertexBoundingBox.y
+          };
+
+          var topEdgeIntersectionPoint = {
+            x: uiVertex.x,
+            y: uiVertex.y + vertexBoundingBox.y
+          };
+
+          var distanceFromLeftToMiddle = GeometryUtils.distanceOfPoints(oldLeftCorner, topEdgeIntersectionPoint);
+          var distanceFromRightToMiddle = vertexBoundingBox.width - distanceFromLeftToMiddle;
+          var distanceFromTopToMiddle = GeometryUtils.distanceOfPoints(uiVertex, topEdgeIntersectionPoint);
+          var distanceFromBottomToMiddle = vertexBoundingBox.height - distanceFromTopToMiddle;
+
+          var leftCorner = {
+            x: d3.event.x + vertexBoundingBox.x,
+            y: d3.event.y + vertexBoundingBox.y
+          };
+
+          var rightCorner = {
+            x: leftCorner.x + vertexBoundingBox.width,
+            y: leftCorner.y + vertexBoundingBox.height
+          };
+
+          if (leftCorner.x <= 0) {
+            uiVertex.x = distanceFromLeftToMiddle;
+          } else if (rightCorner.x >= instanceSettings.width) {
+            uiVertex.x = instanceSettings.width - distanceFromRightToMiddle;
+          } else {
+            uiVertex.x = d3.event.x;
+          }
+
+          if (leftCorner.y <= 0) {
+            uiVertex.y = distanceFromTopToMiddle;
+          } else if (rightCorner.y >= instanceSettings.height) {
+            uiVertex.y = instanceSettings.height - distanceFromBottomToMiddle;
+          } else {
+            uiVertex.y = d3.event.y;
+          }
+
           var graph = uiVertex.vertex.getGraph();
           graph.trigger('vertexDrag', uiVertex);
         }
